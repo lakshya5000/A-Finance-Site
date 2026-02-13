52bf10ae8869e356782ffd054b0e3b4f94d212c0
Lakshya Soni <lakshyasoni657@gmail.com>
2026-02-13 21:58:02 +0530

    Added base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/A-Finance-Site/",
-
+  base: "/A-Finance-Site/",   // 👈 ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
